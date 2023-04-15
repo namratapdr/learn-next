52c6784addf7ef0071d094eecdf4fafc06836d68
namratapdr <namratapodder138@gmail.com>
2023-04-15 22:37:19 +0530

    import image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import ColorHoverButton from "@component/components/buttons/ColorHoverButton";
 import Layout from "@component/components/layout/Layout";
+import sus from "../../public/images/SUS.png"
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,6 +12,10 @@ export default function Home() {
                     In vitae feugiat libero. Suspendisse viverra tincidunt accumsan. Vivamus porta, tortor eget malesuada hendrerit, turpis justo commodo tortor, vitae venenatis nibh enim fermentum dui. Suspendisse potenti. Etiam varius ex orci, in egestas magna vulputate ac. Nulla id ultricies purus. Phasellus vehicula ex a interdum fermentum. Nulla sodales sagittis nulla. Etiam dolor felis, imperdiet eu leo gravida, aliquet eleifend magna.
                 </p>
                 <ColorHoverButton></ColorHoverButton>
+                <div className={`w-full flex justify-center items-center`}>
+                    <Image src={sus} alt="Sussy Baka" className={`my-5 rounded-full`}></Image>
+                </div>
+
             </div>
         </Layout>
     </main>
